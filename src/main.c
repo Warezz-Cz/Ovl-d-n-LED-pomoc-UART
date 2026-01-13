2b6e53f70f45d1d4d53fb641cc36503ae8111641
Warezz-Cz <kvidokrajca@gmail.com>
2026-01-13 11:02:18 +0100

    1.0

--- src/main.c
+++ src/main.c
@@ -5,6 +5,8 @@
 #include "milis.h"
 //#include "delay.h"
 //#include "uart1.h"
+#include "daughterboard.h"
+
 
 void init(void)
 {
@@ -12,26 +14,13 @@ void init(void)
     init_milis();
     //init_uart1();
 
-    GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
-
 }
 
 
 int main(void)
 {
-  
-    uint32_t time = 0;
 
-    init();
 
-    while (1) {
-        if (milis() - time > 333 ) {
-            REVERSE(LED); 
-            time = milis();
-            //printf("%ld\n", time);
-        }
-        //delay_ms(333);
-    }
 }
 
 /*-------------------------------  Assert -----------------------------------*/
